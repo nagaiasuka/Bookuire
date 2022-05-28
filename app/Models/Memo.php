be7460ef61ac2819b08ff8514728b677e99334b9
Nagai Asuka <87313557+nagaiasuka@users.noreply.github.com>
2022-05-28 21:50:09 +0900

    デプロイ準備

--- app/Models/Memo.php
+++ app/Models/Memo.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Models;
 
+namespace App\Models;
+use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
